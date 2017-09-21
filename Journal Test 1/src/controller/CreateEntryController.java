b407980f8b4e394a1624d476ae32187276e88dab
doubledrop <32156970+doubledrop@users.noreply.github.com>
2017-09-21 18:12:49 +1000

    added blah
    blah

--- Journal Test 1/src/controller/CreateEntryController.java
+++ Journal Test 1/src/controller/CreateEntryController.java
@@ -2,7 +2,7 @@
  * TESTING Netbeans
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. blah
  */
 //dragonphlegm test
 package controller;
