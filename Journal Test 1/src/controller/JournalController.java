85cd278684990a19428e5a85e7fb94bfff847ea1
crozza5431 <32156790+crozza5431@users.noreply.github.com>
2017-09-30 16:38:08 +1000

    Update JournalController.java

--- Journal Test 1/src/controller/JournalController.java
+++ Journal Test 1/src/controller/JournalController.java
@@ -7,12 +7,10 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import journal.test.pkg1.JournalTest1;
 import model.Entry;
