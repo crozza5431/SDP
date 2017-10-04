7dea5a0dbb4fee14ea4a0544bd5e23ca368c4b53
crozza5431 <32156790+crozza5431@users.noreply.github.com>
2017-10-04 16:03:22 +1100

    Update ProfileController.java

--- Journal Test 1/src/controller/ProfileController.java
+++ Journal Test 1/src/controller/ProfileController.java
@@ -10,12 +10,12 @@ package controller;
  * @author Caldiddy's PC
  */
 import database.Database;
+import java.io.InvalidObjectException;
 import java.net.URL;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -26,10 +26,8 @@ import javafx.scene.input.MouseButton;
 import javafx.util.Callback;
 import journal.test.pkg1.JournalTest1;
 import model.Journal;
-import model.User;
 
 public class ProfileController implements Initializable {
-    @FXML private Label userProfile;
     @FXML private TableView<Journal> journalTable;
     @FXML private TableColumn<Journal, String> nameClm;
     @FXML private TableColumn<Journal, String> dateClm;
@@ -37,11 +35,7 @@ public class ProfileController implements Initializable {
     @FXML protected void processLogout() {
         JournalTest1.getInstance().userLogout();
     }
-    
-    public final User getUser() {
-        return JournalTest1.getInstance().getLoggedUser();
-    }
-
+   
     @Override
     public void initialize(URL location, ResourceBundle resources) {
       journalTable.setItems(JournalTest1.getInstance().getLoggedUser().getJournals());
@@ -76,9 +70,16 @@ public class ProfileController implements Initializable {
                   rowMenu.show(journalTable, event.getScreenX(), event.getScreenY());
               }
               if (event.getClickCount() == 2 && (! row.isEmpty())) {
-                  Journal rowData = row.getItem();
-                  JournalTest1.getInstance().returnSelected(rowData);
-                  JournalTest1.getInstance().gotoEntry();
+                  try {
+                      Journal rowData = row.getItem();
+                      JournalTest1.getInstance().returnSelected(rowData);
+                      JournalTest1.getInstance().loadEntry();
+                      JournalTest1.getInstance().gotoEntry();
+                  } catch (SQLException ex) {
+                      Logger.getLogger(ProfileController.class.getName()).log(Level.SEVERE, null, ex);
+                  } catch (InvalidObjectException ex) {
+                      Logger.getLogger(ProfileController.class.getName()).log(Level.SEVERE, null, ex);
+                  }
               }
           });
               return row;
