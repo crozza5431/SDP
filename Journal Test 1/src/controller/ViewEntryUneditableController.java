999424ca5d39518a6bd287bb5f8d4dc7b9365b6d
crozza5431 <32156790+crozza5431@users.noreply.github.com>
2017-10-09 15:03:58 +1100

    Update ViewEntryUneditableController.java

--- Journal Test 1/src/controller/ViewEntryUneditableController.java
+++ Journal Test 1/src/controller/ViewEntryUneditableController.java
@@ -5,17 +5,33 @@
  */
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
 import journal.test.pkg1.JournalTest1;
 
 /**
  *
  * @author User
  */
-public class ViewEntryUneditableController {
+public class ViewEntryUneditableController implements Initializable{
+    
+    @FXML Text entryName;
+    @FXML Text entryData;
     
     @FXML protected void handleEditEntry() {
         JournalTest1.getInstance().gotoEditEntry();
     }
     
+    @FXML protected void handleBack() {
+        JournalTest1.getInstance().gotoEntry();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+       entryName.setText(JournalTest1.getInstance().getCurrentEntry().getName());
+       entryData.setText(JournalTest1.getInstance().getCurrentEntry().getEntry());
+    }
 }
