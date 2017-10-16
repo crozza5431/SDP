3b0ac07bc949420ee8dfa269e48598043ca1b253
crozza5431 <32156790+crozza5431@users.noreply.github.com>
2017-10-16 15:12:09 +1100

    Setup for search added

--- Journal Test 1/src/controller/SearchController.java
+++ Journal Test 1/src/controller/SearchController.java
@@ -5,8 +5,12 @@
  */
 package controller;
 
+import java.io.InvalidObjectException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
@@ -14,6 +18,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import journal.test.pkg1.JournalTest1;
 
 /**
@@ -27,15 +32,26 @@ public class SearchController implements Initializable{
     @FXML TableView historyTbl;
     @FXML TableColumn nameClm;
     @FXML TableColumn summaryClm;
+    @FXML TableColumn dateClm;
     @FXML CheckBox historyChbx;
     @FXML CheckBox hiddenChbx;
+    @FXML CheckBox deletedChbx;
+    @FXML Text searchName;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //
+        searchName.setText("Search: " + JournalTest1.getInstance().getJournal().getName());
+        
+        historyChbx.setOnAction(e -> {
+
+        });
+        
+        hiddenChbx.setOnAction(e -> {
+
+        });
     }
     
     @FXML protected void handleBack() {
         JournalTest1.getInstance().gotoProfile();
-    }
+    }    
 }
